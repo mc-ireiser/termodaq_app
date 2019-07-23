84d44d80bc001f1e99c669bedfeb2848ffbd42b5
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-07-23 16:35:26 -0400

    Layouts

---
+++ layouts/tablero.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="app header-fixed sidebar-lg-show sidebar-fixed">
+    <header class="app-header navbar">
+      <!-- Header content here -->
+      <nav class="navbar">
+        <a class="d-flex" href="#">
+          <img src="/img/png/termodaq_logo.png" alt="Logo termoDaQ" height="20">
+        </a>
+        <button class="navbar-toggler sidebar-toggler" type="button" data-toggle="sidebar-show">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </header>
+    <div class="app-body">
+      <div class="sidebar">
+        <!-- Sidebar content here -->
+        <nav class="sidebar-nav" style="overflow-y: auto;">
+          <ul class="nav">
+            <li class="nav-title">Estudios</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-file-alt"></i> Nuevo
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-list-alt"></i> Listado
+              </a>
+            </li>
+            <li class="nav-title">Colaboraciones</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-file-alt"></i> Listado
+              </a>
+            </li>
+            <li class="nav-title">Usuario</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-user"></i> Perfil
+              </a>
+            </li>
+            <li class="nav-item nav-dropdown">
+              <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="fas fa-user-shield"></i> Seguridad
+              </a>
+              <ul class="nav-dropdown-items">
+                <li class="nav-title">Contraseña</li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <i class="fas fa-exchange-alt"></i> Cambiar
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <i class="fas fa-redo"></i> Restablecer
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-sign-out-alt"></i> Salir
+              </a>
+            </li>
+            <li class="nav-item nav-dropdown mt-auto">
+              <a class="nav-link nav-dropdown-toggle nav-link-primary" href="#">
+                <i class="fab fa-github-alt"></i> Github
+              </a>
+              <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                  <a class="nav-link" href="https://github.com/mc-ireiser/termoDaQ" target="_blank">
+                    <i class="fas fa-code-branch"></i> termoDaQ
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://github.com/mc-ireiser/termodaq_firmware_atmega328p" target="_blank">
+                    <i class="fas fa-code-branch"></i> Firmware ATmega328p
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://github.com/mc-ireiser/termodaq_firmware_attiny85" target="_blank">
+                    <i class="fas fa-code-branch"></i> Firmware ATtiny85
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://github.com/mc-ireiser/termodaq_link" target="_blank">
+                    <i class="fas fa-code-branch"></i> LINK
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://github.com/mc-ireiser/termodaq_api" target="_blank">
+                    <i class="fas fa-code-branch"></i> API
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://github.com/mc-ireiser/termodaq_app" target="_blank">
+                    <i class="fas fa-code-branch"></i> APP
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <main class="main">
+        <!-- Main content here -->
+        <nuxt/>
+      </main>
+    </div>
+    <footer class="app-footer">
+      <!-- Footer content here -->
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style>
+</style>
