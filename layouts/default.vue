e83f55955485e4520232881a2e8ad614afdf6c11
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-07-23 11:09:25 -0400

    Inicial

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app header-fixed sidebar-lg-show sidebar-fixed">
+    <header class="app-header navbar">
+      <!-- Header content here -->
+      <nav class="navbar">
+        <a class="d-flex" href="#">
+          <img src="/img/png/termodaq_logo.png" alt="Logo termoDaQ" height="20">
+        </a>
+        <button class="navbar-toggler sidebar-toggler hidden-lg-up" type="button" data-toggle="sidebar-show">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </header>
+    <div class="app-body">
+      <div class="sidebar">
+        <!-- Sidebar content here -->
+      </div>
+      <main class="main">
+        <!-- Main content here -->
+      </main>
+    </div>
+    <footer class="app-footer">
+      <!-- Footer content here -->
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style>
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/bootstrap.min.css' },
+      { rel: 'stylesheet', href: '/css/coreui.min.css' }
+    ],
+    script: [
+      { src: '/js/jquery/3.4.1/jquery-3.4.1.min.js' },
+      { src: '/js/popper/1.15.0/popper.min.js' },
+      { src: '/js/bootstrap/4.3.1/bootstrap.min.js' },
+      { src: '/js/coreui.min.js' },
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    //'bootstrap-vue/nuxt',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
