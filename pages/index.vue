8b238b1bb103346c733953bb1a0dff85504f6826
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-07-24 01:59:47 -0400

    Limpieza

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,6 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        termodaq_app
-      </h1>
-      <h2 class="subtitle">
-        SPC termoDaQ
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
