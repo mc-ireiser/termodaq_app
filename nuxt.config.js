0ea84a2f9fede815a2c854b016875416a32e92ad
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-07-29 15:17:57 -0400

    Revision de codigo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,67 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: '/css/bootstrap.min.css' },
-      { rel: 'stylesheet', href: '/css/coreui.min.css' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "/css/bootstrap.min.css" },
+      { rel: "stylesheet", href: "/css/coreui.min.css" }
     ],
     script: [
-      { src: '/js/jquery/3.4.1/jquery-3.4.1.min.js' },
-      { src: '/js/popper/1.15.0/popper.min.js' },
-      { src: '/js/bootstrap/4.3.1/bootstrap.min.js' },
-      { src: '/js/coreui.min.js' },
-      { src: '/js/fontawesome/fontawesome-3653f64de2.js' }
+      { src: "/js/jquery/3.4.1/jquery-3.4.1.min.js" },
+      { src: "/js/popper/1.15.0/popper.min.js" },
+      { src: "/js/bootstrap/4.3.1/bootstrap.min.js" },
+      { src: "/js/coreui.min.js" },
+      { src: "/js/fontawesome/fontawesome-3653f64de2.js" }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
     //'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/toast',
-    'nuxt-leaflet'
+    "@nuxtjs/axios",
+    "@nuxtjs/toast",
+    "nuxt-leaflet"
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
